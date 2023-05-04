21bfa1b90d349ed364a65d7120381a481db3431d
Abdelrahman Mohamed <11410120210229@stud.cu.edu.eg>
2023-05-04 15:53:14 +0300

    Ab1

---
+++ ToffeStore/src/Cash_Method.java
@@ -0,0 +1,6 @@
+public class Cash_Method implements Pay_Method {
+    @Override
+    public void Redeem() {
+
+    }
+}
---
+++ ToffeStore/src/Shopping_Cart.java
@@ -0,0 +1,11 @@
+public class Shopping_Cart {
+    private String[] items;
+    private double totalPrice;
+    public boolean addItem(String item){
+        return false;
+    }
+    private double calculateTotal(){
+        return 0;
+    }
+    public void checkOut(){}
+}
