8580983507eeca90617760e0b62253564b2f7d5b
Abdelrahman Mohamed <11410120210229@stud.cu.edu.eg>
2023-05-06 13:14:12 +0300

    Ab2

--- ToffeStore/src/Order.java
+++ ToffeStore/src/Order.java
@@ -2,17 +2,18 @@ import java.util.ArrayList;
 
 public class Order {
     private ArrayList<Item> items;
-    private  static String lastOrderID;
+    private  static String lastOrderID = "0";
     private  String orderID;
     private String userID;
     private double totalPrice;
     private Pay_Method payMethod;
     private String Address;
-    public Order(ArrayList<Item> items, String userID, Pay_Method payMethod, String Address){
-        this.items = items;
+    public Order(Shopping_Cart cart, String userID, Pay_Method payMethod, String Address){
+        this.items = cart.getItems();
+        this.totalPrice = cart.calculateTotal();
         this.userID = userID;
         this.payMethod = payMethod;
         this.Address = Address;
-        this.orderID = lastOrderID + 1;
+        this.orderID = lastOrderID + "1";
     }
 }
--- ToffeStore/src/Shopping_Cart.java
+++ ToffeStore/src/Shopping_Cart.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Shopping_Cart {
     private ArrayList<Item> items;
@@ -16,6 +18,29 @@ public class Shopping_Cart {
         if (item.isAvailable()){
             items.add(item);
             return true;
+        }
+        return false;
+    }
+    public  ArrayList getItems (){
+        return items;
+    }
+
+    // checkOut in user
+    public boolean chekOut(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose Pay Method to Continue: ");
+        System.out.println("1-Cash ");
+        String ans = input.nextLine();
+        if (ans == "1" || ans.toLowerCase(Locale.ROOT) == "cash") {
+            Pay_Method payMethod = new Cash_Method();
+            Order order = new Order(cart, userID, payMethod,Address);
+            orders.add(order);
+            payMethod.Redeem();
+            System.out.println("Order Taken Successfully");
+            return true;
+        }
+        else {
+            System.out.println("Sorry This Method Not Available Now");
         }
         return false;
     }
